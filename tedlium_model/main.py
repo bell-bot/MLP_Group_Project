d3f87aa6f776047daf17b1f807afd2af4a6a4ee4
SzymonCiciala <47600083+s-ciciala@users.noreply.github.com>
2022-03-24 20:48:56 +0000

    os GPU varibales

--- tedlium_model/main.py
+++ tedlium_model/main.py
@@ -4,6 +4,9 @@ import yaml
 import torch
 import argparse
 import numpy as np
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
 
 # For reproducibility, comment these may speed up training
 torch.backends.cudnn.deterministic = True
