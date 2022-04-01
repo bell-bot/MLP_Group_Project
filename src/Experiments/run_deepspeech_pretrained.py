ff959acf515d9c535153b37c3fa8ae213a34f5e9
wassim2u <wassim.jabrane@gmail.com>
2022-04-01 07:10:48 +0100

    Test keywords inference + deepspeech on adversarial code

--- src/Experiments/run_deepspeech_pretrained.py
+++ src/Experiments/run_deepspeech_pretrained.py
@@ -7,12 +7,13 @@ from tqdm import tqdm
 import tensorflow as tf
 import IPython
 import time
+import pandas as pd
+import traceback
 
-from src.constants import DATASET_TEDLIUM_PATH, DATASET_MLCOMMONS_PATH
+from src.constants import DATASET_TEDLIUM_PATH, DATASET_MLCOMMONS_PATH, ADVERSERIAL_DATASET_PATH, LABELS_WITH_ADVERSERIAL
 
 ted_filename  = "ted_temp.wav"
 keyword_filename = "keyword_temp.wav"
-
 def main():
     wrapper = CTRLF_DatasetWrapper()
     samples_to_write= sorted(list(wrapper.TED_sampleids_in_labels_set))
@@ -126,11 +127,145 @@ def main():
     acc = open("pretrained_stats.txt", "w")
     runtimes = open("pretrained_runtimes.csv", "w")
     runtimes.write("TED_Length,Runtimes\n")
+    acc.writelines([str(final_accuracy), str(avg_runtime)])
+    for (key,value) in ted_lengths.items():
+        runtimes.write(f"{key},{value}\n")
+    runtimes.close()
+    acc.close()
+
+    print("Model accuracy: ", sum(accuracy)/len(accuracy))
+    print("Average runtime per prediction:", avg_runtime)
+    print("Model Done.")
+
+
+class AdverserialDataset:
+    def __init__(self, adverserial_labels_path= LABELS_WITH_ADVERSERIAL):
+        self.labels_path = adverserial_labels_path
+        self.labels = pd.read_csv(self.labels_path)
+        self.adverserial_TED_talk_ids = sorted(list(self.labels.TEDLIUM_SampleID.unique()))
+
+        
+
+
+def main_adverserial():
+    print("Evaluating Adversarials")
+    wrapper = CTRLF_DatasetWrapper()
+    AdverserialWrapper = AdverserialDataset()
+    adversarial_labels_df = AdverserialWrapper.labels
+    samples_to_write= sorted(list(AdverserialWrapper.adverserial_TED_talk_ids))
+    
+    accuracy = []
+    runtime = []
+    ted_lengths = {}
+    acc_file = open("test_acc_adverserial.txt", "w")
+    run_file = open("runtime_adverserial.txt", "w")
+    transcriptions_original_ted_vs_adversarial = open("adv_vs_original_ted_transcripts.txt", "w")
+    transcriptions_original_ted_vs_adversarial.write("TED_transcipt,Adverserial_Inferred_Transcript\n")
+    for i in tqdm(range(len(samples_to_write))):
+        try:
+            runtime_per_ted = []
+            start_timer = time.perf_counter()
+            idx = samples_to_write[i]
+            # Retrieve the sample at the current index
+            sample = wrapper.get(idx)
+            keyword_start_times = sample["keyword_start_time"].tolist()
+            keyword_end_times = sample["keyword_end_time"].tolist()
+
+            # Retrieve the adverserial waveform at the current index
+            ted_sample_dict = wrapper.TED.__getitem__(idx)
+            ted_waveform = ted_sample_dict["waveform"]
+            ted_sample_rate = ted_sample_dict["sample_rate"]
+            ted_start_time = ted_sample_dict["start_time"]
+            ted_end_time = ted_sample_dict["end_time"]
+            ted_length = ted_end_time-ted_start_time
+            original_ted_transcript = ted_sample_dict["transcript"]
+            adverserial_waveform = adversarial_labels_df[adversarial_labels_df["TEDLIUM_SampleID"] == idx].TEDLIUM_SET.iloc[0] + ".wav"
+            adverserial_waveform = os.path.join(ADVERSERIAL_DATASET_PATH, adverserial_waveform)
+      
+
+            # Transcribe the ted sample
+            adv_transcript, ted_timestamps = dsp.main("deepspeech-0.9.3-models.pbmm", adverserial_waveform, scorer="deepspeech-0.9.3-models.scorer")
+
+            # Get the keyword ids for all keywords
+            keywords = sample["MSWC_ID"].tolist()
+
+            # Iterate over all keywords trying to detect them
+            iterator = 0
+            for keyword in keywords:
+                # Retrieve keyword and information
+                keyword = wrapper.MSWC.__getitem__(keyword)
+                keyword_waveform = keyword["waveform"]
+                keyword_waveform = keyword_waveform.reshape(keyword_waveform.shape[0],1)
+                keyword_sample_rate = keyword["sample_rate"]
+                keyword_start_time = keyword_start_times[iterator]
+                keyword_end_time = keyword_end_times[iterator]
+                iterator += 1
+
+                # Save to wav file
+                encode = tf.audio.encode_wav(
+                keyword_waveform, keyword_sample_rate, name=None
+                )
+                tf.io.write_file(
+                keyword_filename, encode, name=None
+                )
+            
+                # transcribe the keyword
+                keyword_transcript, keyword_timestamps = dsp.main("deepspeech-0.9.3-models.pbmm", keyword_filename, scorer="deepspeech-0.9.3-models.scorer")
+
+                #keyword_transcript = keyword_transcript.strip()
+            
+                keyword_len = len(keyword_transcript)
+                i = 0
+
+                print(f"Trying to identify the keyword:++{keyword_transcript}++ within transcript:++{adv_transcript}++.")
+                print(f"Original transcript: {original_ted_transcript}")
+                transcriptions_original_ted_vs_adversarial.write(f"{original_ted_transcript},{adv_transcript}\n")
+
+                # Match the transcripts
+                while True:
+                    try:
+                        transcript_snippet = str(adv_transcript[i:i+keyword_len])
+                        if transcript_snippet == keyword_transcript:
+                            start = ted_start_time+ted_timestamps[i]
+                            end = ted_start_time+ted_timestamps[i+keyword_len]
+                            print(start,end,keyword_start_time, keyword_end_time)
+                            accuracy.append(correct_timestamp(start, end, keyword_start_time, keyword_end_time))
+                            acc_file.write(str(correct_timestamp(start, end, keyword_start_time, keyword_end_time)) + "\n")
+                            break
+                        i += 1
+                        if i > len(adv_transcript):
+                            accuracy.append(False)
+                            #acc.write("False\n") - removed because not used 
+                            break
+                    except  Exception as e:
+                        print(traceback.format_exc())
+                        break
+                
+                end_timer = time.perf_counter()
+                runtime.append(end_timer-start_timer)
+                run_file.write(f"{ted_length},{end_timer-start_timer}\n")
+                runtime_per_ted.append(end_timer-start_timer)
+                start_timer = end_timer
+                if ted_length in ted_lengths.keys():
+                    runtimes = ted_lengths.get(ted_length)
+                    ted_lengths[ted_length] = runtimes + runtime_per_ted
+        except Exception as e:
+            print(traceback.format_exc())
+            continue
+            
+            
+
+    final_accuracy = sum(accuracy)/len(accuracy)
+    avg_runtime = sum(runtime)/len(runtime)
+    acc = open("pretrained_adverserial_stats.txt", "w")
+    runtimes = open("pretrained_adverserial_runtimes.csv", "w")
+    runtimes.write("TED_Length,Runtimes\n")
     acc.writelines([str(final_accuracy), avg_runtime])
     for (key,value) in ted_lengths.items():
         runtimes.write(f"{key},{value}\n")
     runtimes.close()
     acc.close()
+    transcriptions_original_ted_vs_adversarial.close()
 
     print("Model accuracy: ", sum(accuracy)/len(accuracy))
     print("Average runtime per prediction:", avg_runtime)
@@ -145,6 +280,7 @@ def correct_timestamp(start, end, keyword_start, keyword_end):
 
     return start_before or end_after
 
-main()
+# main()
+main_adverserial()
 # y hamster has 8gb ram
 
--- src/models/test_model.py
+++ src/models/test_model.py
@@ -11,14 +11,17 @@ from IPython import display
 from jiwer import wer
 import csv
 import os
+import librosa
 from src.utils import get_git_root
 from tqdm import tqdm
 from src.datasets import CTRLF_DatasetWrapper
 from Data import data_utils
 from moviepy.audio.AudioClip import AudioArrayClip
-
+from src.datasets import MultiLingualSpokenWordsEnglish
 metadata_path = "/home/szy/Documents/MLP_Group_Project/src/metadata.csv"
 wav_path = "/Users/Wassim/Documents/Year 4/MLP/CW3:4/MLP_Group_Project/Data/TEDLIUM_release-3/data/wav/"
+keyword_path = MultiLingualSpokenWordsEnglish.MSWC_EN_AUDIO_FOLDER + "/"
+
 model_directory = "/Users/Wassim/Documents/Year 4/MLP/CW3:4/MLP_Group_Project/src/models/"
 generated_text_stats_path = "/Users/Wassim/Documents/Year 4/MLP/CW3:4/MLP_Group_Project/src/models/"
 generated_history_path = "/Users/Wassim/Documents/Year 4/MLP/CW3:4/MLP_Group_Project/src/models/"
@@ -38,12 +41,12 @@ NUM_OF_SAMPLES = 3000 #<---- DATASET SIZE
 BATCH_SIZE = 16
 RNN_UNITS= 128 #original: 512
 RNN_LAYERS = 2 #original : 5
-LR_ADAM = 1e-3
+LR_ADAM = 1e-4
 
 EPOCHS =100
 
 TEST_START =NUM_OF_SAMPLES
-TEST_END = NUM_OF_SAMPLES + 400
+TEST_END = NUM_OF_SAMPLES + 300
 # ------------------------------------------
 
 print(tf.__version__)
@@ -51,15 +54,6 @@ print(tf.test.gpu_device_name())
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
 CTRLF_Engine = CTRLF_DatasetWrapper()
-
-def read_dataset_metadata():
-    # Read metadata file and parse it
-    metadata_df = pd.read_csv(metadata_path)
-    metadata_df.columns = ["TED_waveform","start_time","end_time","TED_transcript"]
-    metadata_df = metadata_df[["TED_waveform","TED_transcript"]]
-    metadata_df = metadata_df.sample(frac=1).reset_index(drop=True)
-    metadata_df.head(3)
-    return metadata_df
 
 def read_ctrlf_dataset(start_sample= 0, num_of_samples=3000, if_keyword=False):
     audio_df = pd.DataFrame(columns=CTRLF_Engine.COLS_OUTPUT_TED_SIMPLIFIED)
@@ -71,7 +65,7 @@ def read_ctrlf_dataset(start_sample= 0, num_of_samples=3000, if_keyword=False):
         if len(sample) ==0:
             num_of_samples+=1
             continue
-        else:
+        else: #TODO: Go through each keyword
             ted_waveform = sample["TED_waveform"][0]
             ted_waveform = ted_waveform.reshape(ted_waveform.shape[1],1)
             keyword_audio = sample["MSWC_audio_waveform"][0]
@@ -84,11 +78,12 @@ def read_ctrlf_dataset(start_sample= 0, num_of_samples=3000, if_keyword=False):
             keyword_start_time = sample["keyword_start_time"][0]
             keyword_end_time = sample["keyword_end_time"][0]
             ted_transcript = sample["TED_transcript"][0]
-            ted_talk_id = sample["talk_id"][0]
+            ted_talk_id = sample["TED_talk_id"][0]
 
             keyword_id = sample["MSWC_ID"][0]
+            row = None
             if if_keyword:
-                row = [keyword_id, sample["keyword"]]
+                row = [keyword_id, sample["keyword"][0]]
             else:
                 row = [ted_talk_id, ted_transcript]
                 row[0]= str(i) + "_" + row[0]
@@ -107,39 +102,15 @@ def read_ctrlf_dataset(start_sample= 0, num_of_samples=3000, if_keyword=False):
     return audio_df
   
   
-def read_ctrlf_dataset_keyword(num_of_samples=3000):
-    audio_df = pd.DataFrame(columns=CTRLF_Engine.COLS_OUTPUT_TED_SIMPLIFIED)
-    output_rows = []
-    for i in tqdm(range(0,num_of_samples),desc="Preparing Dataset"):
-        row = CTRLF_Engine.get_ted_talk_id_and_transcript(i)
-        
-        if len(row) ==0:
-            num_of_samples+=1
-            continue
-        else:
-            row[0]= str(i) + "_" + row[0]
-            if REMOVE_UNK:
-                row[1] = row[1].replace("<unk>", "") #TODO: See if this is plausible
-            if PREPROCESS:
-                row[1] = data_utils.preprocess_text(row[1])
-                try:
-                    tokens = [data_utils.parse_number_string(word) for word in row[1].split()]
-                    row[1] = " ".join(tokens)
-                except:
-                    continue
-            output_rows.append(row)
-    audio_df = pd.DataFrame(data= output_rows, columns=["TED_Talk_ID", "TED_transcript"])
-    audio_df.reset_index(inplace=True, drop=True)
-    return audio_df      
 
-def encode_single_sample(wav_file, label):
+def encode_single_sample(opus_file, label):
     ###########################################
     ##  Process the Audio
     ##########################################
      # 1. Read wav file
-    file = tf.io.read_file(wav_path + wav_file + ".wav")
+    file = tf.io.read_file(opus_file)
     # 2. Decode the wav file
-    audio, _ = tf.audio.decode_wav(file)
+    audio, _ = tf.audio.decode_vorbis(file)
     audio = tf.squeeze(audio, axis=-1)
     # 3. Change type to float
     audio = tf.cast(audio, tf.float32)
@@ -192,12 +163,48 @@ def decode_batch_predictions(pred):
     return output_text
 
 
-metadata_df = read_ctrlf_dataset(start_sample= TEST_START, num_of_samples=TEST_END)
-print(metadata_df[0:10])
+# metadata_df = read_ctrlf_dataset(start_sample= TEST_START, num_of_samples=TEST_END)
+# print(metadata_df[0:10])
 # df_train = metadata_df[:split]
-df_val = metadata_df
+# df_val = metadata_df
 # print(f"Size of the training set: {len(df_train)}")
-print(f"Size of the test set: {len(df_val)}")
+# print(f"Size of the test set: {len(df_val)}")
+# The set of characters accepted in the TED_transcription.
+
+# characters = [x for x in "abcdefghijklmnopqrstuvwxyz'?! "]
+# # Mapping characters to integers
+# char_to_num = keras.layers.StringLookup(vocabulary=characters, oov_token="")
+
+# # Mapping integers back to original characters
+# num_to_char = keras.layers.StringLookup(
+#     vocabulary=char_to_num.get_vocabulary(), oov_token="", invert=True)
+
+# print(
+#     f"The vocabulary is: {char_to_num.get_vocabulary()} "
+#     f"(size ={char_to_num.vocabulary_size()})"
+# )
+
+
+
+# # Define the validation dataset
+# validation_dataset = tf.data.Dataset.from_tensor_slices(
+#     (list(df_val["TED_Talk_ID"]), list(df_val["TED_transcript"]))
+# )
+# validation_dataset = (
+#     validation_dataset.map(encode_single_sample, num_parallel_calls=tf.data.AUTOTUNE)
+#     .padded_batch(BATCH_SIZE)
+#     .prefetch(buffer_size=tf.data.AUTOTUNE)
+# )
+
+# -----------------
+
+
+metadata_keyword_df = read_ctrlf_dataset(start_sample= TEST_START, num_of_samples=TEST_END, if_keyword=True)
+print(metadata_keyword_df[0:10])
+# df_train = metadata_df[:split]
+df_val_keyword = metadata_keyword_df
+# print(f"Size of the training set: {len(df_train)}")
+print(f"Size of the test set: {len(df_val_keyword)}")
 # The set of characters accepted in the TED_transcription.
 
 characters = [x for x in "abcdefghijklmnopqrstuvwxyz'?! "]
@@ -213,58 +220,46 @@ print(
     f"(size ={char_to_num.vocabulary_size()})"
 )
 
-# Define the trainig dataset
 
-
-train_dataset = tf.data.Dataset.from_tensor_slices(
-    (list(df_train["TED_Talk_ID"]), list(df_train["TED_transcript"]))
-)
-train_dataset = (
-    train_dataset.map(encode_single_sample, num_parallel_calls=tf.data.AUTOTUNE)
-    .padded_batch(BATCH_SIZE)
-    .prefetch(buffer_size=tf.data.AUTOTUNE)
-)
 
 # Define the validation dataset
-validation_dataset = tf.data.Dataset.from_tensor_slices(
-    (list(df_val["TED_Talk_ID"]), list(df_val["TED_transcript"]))
+validation_keyword_dataset = tf.data.Dataset.from_tensor_slices(
+    (list(df_val_keyword["TED_Talk_ID"]), list(df_val_keyword["TED_transcript"]))
 )
-validation_dataset = (
-    validation_dataset.map(encode_single_sample, num_parallel_calls=tf.data.AUTOTUNE)
+validation_keyword_dataset = (
+    validation_keyword_dataset.map(encode_single_sample, num_parallel_calls=tf.data.AUTOTUNE)
     .padded_batch(BATCH_SIZE)
     .prefetch(buffer_size=tf.data.AUTOTUNE)
 )
+
 
 #Disable Auto Sharding
 options = tf.data.Options()
 options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.OFF
-train_dataset = train_dataset.with_options(options)
-validation_dataset = validation_dataset.with_options(options)
+# validation_dataset = validation_dataset.with_options(options)
+validation_keyword_dataset = validation_keyword_dataset.with_options(options)
 
-
-fig = plt.figure(figsize=(8, 5))
-for batch in train_dataset.take(1):
-    spectrogram = batch[0][0].numpy()
-    spectrogram = np.array([np.trim_zeros(x) for x in np.transpose(spectrogram)])
-    label = batch[1][0]
-    # Spectrogram
-    label = tf.strings.reduce_join(num_to_char(label)).numpy().decode("utf-8")
-    ax = plt.subplot(2, 1, 1)
-    ax.imshow(spectrogram, vmax=1)
-    ax.set_title(label)
-    ax.axis("off")
-    # Wav
-    file = tf.io.read_file(wav_path + list(df_train["TED_Talk_ID"])[0] + ".wav")
-    audio, _ = tf.audio.decode_wav(file)
-    audio = audio.numpy()
-    ax = plt.subplot(2, 1, 2)
-    plt.plot(audio)
-    ax.set_title("Signal Wave")
-    ax.set_xlim(0, len(audio))
-    display.display(display.Audio(np.transpose(audio), rate=16000))
-plt.show()
-
-
+# fig = plt.figure(figsize=(8, 5))
+# for batch in train_dataset.take(1):
+#     spectrogram = batch[0][0].numpy()
+#     spectrogram = np.array([np.trim_zeros(x) for x in np.transpose(spectrogram)])
+#     label = batch[1][0]
+#     # Spectrogram
+#     label = tf.strings.reduce_join(num_to_char(label)).numpy().decode("utf-8")
+#     ax = plt.subplot(2, 1, 1)
+#     ax.imshow(spectrogram, vmax=1)
+#     ax.set_title(label)
+#     ax.axis("off")
+#     # Wav
+#     file = tf.io.read_file(wav_path + list(df_train["TED_Talk_ID"])[0] + ".wav")
+#     audio, _ = tf.audio.decode_wav(file)
+#     audio = audio.numpy()
+#     ax = plt.subplot(2, 1, 2)
+#     plt.plot(audio)
+#     ax.set_title("Signal Wave")
+#     ax.set_xlim(0, len(audio))
+#     display.display(display.Audio(np.transpose(audio), rate=16000))
+# plt.show()
 
 model = tf.keras.models.load_model(model_folder_path, compile=False)
 
@@ -285,12 +280,43 @@ def eval_model(num_of_predictions_to_print=5):
     # tf.saved_model.save(model, model_directory)
     wer_score = wer(targets, predictions)
     
-    with open(model_directory + "model_stats.txt", "a") as f:  
+    with open(model_directory + "model_ted_stats_test.txt", "a") as f:  
         print("-" * 100)
         print(f"Word Error Rate: {wer_score:.4f}")
         f.write("WER: " + str(wer_score) +"\n")
         print("-" * 100)
-        for i in np.random.randint(0, len(predictions), num_of_predictions_to_print):
+        for i in range(0, 10):
+            print(f"Target    : {targets[i]}")
+            print(f"Prediction: {predictions[i]}")
+            print("-" * 100)
+
+            f.write(f"Target: {targets[i]}\n")
+            f.write(f"Prediction: {predictions[i]}\n")
+            
+
+
+def eval_model_keywords(num_of_predictions_to_print=5):
+    
+    # Let's check results on more validation samples
+    predictions = []
+    targets = []
+    for batch in validation_keyword_dataset:
+        X, y = batch
+        batch_predictions = model.predict(X)
+        batch_predictions = decode_batch_predictions(batch_predictions)
+        predictions.extend(batch_predictions)
+        for label in y:
+            label = tf.strings.reduce_join(num_to_char(label)).numpy().decode("utf-8")
+            targets.append(label)
+    # tf.saved_model.save(model, model_directory)
+    wer_score = wer(targets, predictions)
+    
+    with open(model_directory + "model_keywords_stats_test.txt", "a") as f:  
+        print("-" * 100)
+        print(f"Word Error Rate: {wer_score:.4f}")
+        f.write("WER: " + str(wer_score) +"\n")
+        print("-" * 100)
+        for i in range(0, 10):
             print(f"Target    : {targets[i]}")
             print(f"Prediction: {predictions[i]}")
             print("-" * 100)
@@ -303,14 +329,18 @@ def eval_model(num_of_predictions_to_print=5):
 
 
 
-
-
 # ------------------------------------------------
         
         
 # Let's check results on more validation samples
-eval_model()
-
+import time 
+# start_time_perf = time.perf_counter()
+# eval_model()
+# print((start_time_perf - time.perf_counter())/len(validation_dataset))
+ 
+start_time_perf = time.perf_counter()
+eval_model_keywords()
+print(start_time_perf - time.perf_counter()/len(validation_dataset)) 
 
 
     
